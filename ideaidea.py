26eefcfcde9cb96e8dc0e0d202d33af797106795
GammelDanks <d.native@gmx.de>
2025-03-08 21:21:08 +0100

    Update ideaidea.py

--- ideaidea.py
+++ ideaidea.py
@@ -3,7 +3,7 @@ import openai
 import time
 
 # Set the OpenAI API key
-openai.api_key = st.secrets["openai"]["openai_api_key"]
+openai.api_key = st.secrets["general"]["openai_api_key"]
 
 # Function to generate innovative solutions
 def generate_innovative_solutions(problem_description, target_audience):
